358a6795899c4629e93ef7d7e9a8d00bb59139ab
Maryan Fawzy <163450742+maryanfawzy@users.noreply.github.com>
2025-03-19 20:20:36 -0500

    story

---
+++ src/components/Story.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Story = () => {
+  return (
+    <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
+        <div className='flex '>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Story
