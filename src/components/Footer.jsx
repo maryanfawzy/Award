8978732da35836fb6df945ac0764dda5e68caecd
Maryan Fawzy <163450742+maryanfawzy@users.noreply.github.com>
2025-03-21 00:00:00 -0500

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { FaDiscord, FaGithub } from 'react-icons/fa'
+import { FaDiscord, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { FaPerson } from 'react-icons/fa6'
 import { HiRefresh } from 'react-icons/hi'
 const links = [
-    {href: 'https://maryanfawzy.com', icon : <FaDiscord/>},
-    {href: 'https://maryanfawzy.com', icon : <FaDiscord/>},
-    {href: 'https://maryanfawzy.com', icon : <FaGithub/>}
+    {href: 'https://maryanfawzy.com', icon : <FaPerson/>},
+    {href: 'https://www.linkedin.com/in/maryan-fawzy-591a0990', icon : <FaLinkedin/>},
+    {href: 'https://github.com/maryanfawzy', icon : <FaGithub/>}
 ]
 
 const Footer = () => {
@@ -13,10 +14,15 @@ const Footer = () => {
         <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
             <p className="text-center text-sm font-light md:text-left">@Maryan Fawzy 2025</p>
             <div className="flex justify-center gap-4  md:justify-start">
+                {links.map((link)=>(
+                    <a key={link} href={link.href} target='-blank'
+                    rel='noopener noreferrer '  className="text-black transition-colors duration-500 ease-in-out hover:text-white">{link.icon}</a>
+                ))}
                 
 
 
             </div >
+            <a href=""></a>
 
 
         </div>
